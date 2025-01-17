4d34571fceed9ca6e6adbe84436e76609ef6704b
Hlaing Min Aung <hlaingmianung2580@gmail.com>
2025-01-17 10:29:26 +0700

    modified

--- src/web.ts
+++ src/web.ts
@@ -1,5 +1,4 @@
-import { WebPlugin } from '@capacitor/core';
-
+import { WebPlugin, PluginListenerHandle } from '@capacitor/core';
 import type { ScreenRecorderPlugin } from './definitions';
 
 export class ScreenRecorderWeb
@@ -8,21 +7,34 @@ export class ScreenRecorderWeb
 {
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log('ECHO', options);
-    return options;
+    return { value: options.value };
   }
 
   async start(options: any): Promise<any> {
     console.log('startFunctionCalled', options);
-    throw new Error("Method not implemented.");
+    throw this.unimplemented('The start method is not implemented on the web platform.');
   }
 
   async stop(options: any): Promise<any> {
     console.log('stopFunctionCalled', options);
-    throw new Error("Method not implemented.");
+    throw this.unimplemented('The stop method is not implemented on the web platform.');
   }
 
   async recorder_status(options: any): Promise<any> {
     console.log('recorderStatusFunctionCalled', options);
-    throw new Error("Method not implemented.");
+    throw this.unimplemented('The recorder_status method is not implemented on the web platform.');
+  }
+
+  addListener(
+    eventName: string,
+    listenerFunc: (event: any) => void,
+  ): Promise<PluginListenerHandle> & PluginListenerHandle {
+    const handle = super.addListener(eventName, listenerFunc);
+    return Object.assign(handle, {
+      remove: async () => {
+        console.log(`Listener for ${eventName} removed`);
+        (await handle).remove();
+      },
+    });
   }
 }
